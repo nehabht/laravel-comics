630a1e4b48edc1d463bb274ecd0163c51e9db34b
nehabht <ne.bhatti2@icloud.com>
2022-06-09 16:32:03 +0200

    add header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,36 @@
+<header id="site_header">
+    <div class="top_header">
+        <div class="container d-flex justify-content-end align-items-center">
+            <a href="#" class="pe-5">DC POWER™ VISA®</a>
+            <a href="#">ADDITIONAL DC SITES<i class="fas fa-chevron-down ps-2"></i></a>
+        </div>
+    </div>
+    <div class="container">
+        <div class="menu_nav d-flex justify-content-between align-items-center">
+            <img src="{{asset('img/dc-logo.png')}}" alt="" class="logo_img" />
+
+
+            <nav class="menu">
+                <a href="">CHARACTERS</a>
+                <a href="" class="active_text">COMICS</a>
+                <a href="">MOVIES</a>
+                <a href="">TV</a>
+                <a href="">GAMES</a>
+                <a href="">COLLECTIBLES</a>
+                <a href="">VIDEOS</a>
+                <a href="">FANS</a>
+                <a href="">NEWS</a>
+                <a href="">SHOP</a>
+
+            </nav>
+
+            <section>
+                <span class="search_bar">Search</span>
+            </section>
+
+
+        </div>
+    </div>
+
+</header>
+<!-- /#site_header -->
